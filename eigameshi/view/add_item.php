0aefaf3e968ece7e6703526284efb75d31d40367
miwako <miwakocode@gmail.com>
2017-09-07 14:53:04 +0000

    商品登録ページ画像挿バグ修正

--- eigameshi/view/add_item.php
+++ eigameshi/view/add_item.php
@@ -2,7 +2,6 @@
 <html lang="ja">
 
 <head>
-<<<<<<< HEAD
     <meta charset="UTF-8">
     <title>映画飯{管理}</title>
     <link rel="stylesheet" href="html5reset-1.6.1.css">
@@ -27,72 +26,6 @@
             border-bottom-style:ridge;
         }
     </style>
-=======
-<meta charset="UTF-8">
-<title>映画飯{管理}</title>
-<link rel="stylesheet" href="html5reset-1.6.1.css">
-<link rel="stylesheet" href="view/movie.css">
-<link rel="icon" href="./img/icon/favicon.ico" type="image/png"
-	sizes="18x18">
-<meta name="description"
-	content="いつもの食卓をちょっぴり豪勢に！主婦からの投稿が多いので、誰でも作れる簡単レシピが沢山掲載されております！">
-<meta name="keywords" content="CookCamp,クックキャンプ,レシピ,献立,簡単,料理,recipe">
-
-<title>[管理飯]</title>
-<style>
-.status_false {
-	background-color: #A9A9A9;
-}
-.now_item {
-	display: block;
-	font-size: 0.5ex;
-	text-align: left;
-	width:500px;
-		margin:0;
-}
-a.now_item {
-	display:block;
-	font-size: 0.5ex;
-	text-align: left;
-	width:100px;
-	margin:0;
-	padding:0px;
-}
-    section {
-      margin-bottom: 20px;
-      border-top: solid 1px;
-      width: 800px;
-    }
-
-    table {
-      border-collapse: collapse;
-      text-align: left;
-    }
-    tr{
-      border-bottom: solid 1px;
-      padding: 10px;
-      text-align:center;
-      text_align_left:0;
-    }
-
-    caption {
-      text-align: left;
-    }
-
-    .text_align_right {
-      text-align: right;
-    }
-
-    .item_name_width {
-      width: 100px;
-    }
-
-    .input_text_width {
-      width: 60px;
-    }
-
-</style>
->>>>>>> 48365b6... コミットメッセージ
 </head>
 <body>
     <?php if (empty($result_msg) !== TRUE) { ?>
@@ -106,7 +39,6 @@ a.now_item {
     </p>
     <?php } ?>
     <h1>商品管理ツール</h1>
-<<<<<<< HEAD
     <section>
         <h2>新規商品追加</h2>
         <form method="post" enctype="multipart/form-data">
@@ -136,57 +68,12 @@ a.now_item {
                 <th></th>
                 <th></th>
             </tr>
-=======
-	<section>
-		<h2>新規商品追加</h2>
-		<form method="post" enctype="multipart/form-data">
-			<div>
-				<label>名前: <input type="text" name="new_name" value=""></label>
-			</div>
-			<div>
-				<label>値段: <input type="text" name="new_price" value=""></label>
-			</div>
-			<div>
-				<label>個数: <input type="text" name="new_stock" value=""></label>
-			</div>
-			<div>
-				<input type="file" name="new_img">
-			</div>
-			<input type="hidden" name="sql_kind" value="insert">
-			<div>
-				<label>ステータス： <select name="new_status">
-						<option value="0">非公開</option>
-						<option value="1">公開</option>
-				</select></label>
-			</div>
-			<div>
-				<input type="submit" value="商品を追加">
-			</div>
-		</form>
-	</section>
-	<section>
-		<table>
-			<caption>
-					商品情報
-			</caption>
-			<tr>
-				<th>登録商品情報</th>
-				<th>画像</th>
-				<th>商品名</th>
-				<th>価格</th>
-				<th>在庫数</th>
-				<th>削除</th>
-				<th>公開</th>
-			</tr>
->>>>>>> 48365b6... コミットメッセージ
             <?php foreach ($data as $value)  { ?>
             <?php if ( $value['status'] === '1' ) { ?>
             <tr>
                 <?php } else { ?>
-			
-			<tr class="status_false">
+                <tr class="status_false">
                     <?php } ?>
-<<<<<<< HEAD
                     <th>
                         <img src="<?php print $img_dir . $value['img']; ?>" height="100" width="100"></br>
                     </th>
@@ -231,77 +118,27 @@ a.now_item {
                         </form>
                     </th>
                     <th>
-=======
-                <th class="now_item"><img
-					src="<?php print $img_dir . $value['img']; ?>" height="100" width="100"></br>
-				     <a class="now_item">商品名:</a><a><?php print $value['item_name']; ?></a></br> 
-                     <a class="now_item">商品価格：<?php print $value['price']; ?>円</a> </br>
-					 <a class="now_item">商品在庫：<?php print $value['stock']; ?>個</a>
-				</th>
-		<th>
-             <form method="post" enctype="multipart/form-data">  
-                <input type="file" name="update_img" img src="<?php print $img_dir . $value['img']; ?>"
-                <input type="hidden" name="sql_kind" value="update_img">
-                <input type= "hidden" name="item_id" value="<?php print $value['item_id']; ?>">
-                <div><input type="submit" value="画像を変更"></div>
-             </form>
-         </th>
-         <th>
-             <form method="post">
-                <input type="text" name="item_name"> 
-                <input type="submit"  value= "商品名変更" >
-                <input type="hidden" name="sql_kind" value="update_item_name">
-                <input type= "hidden" name="item_id" value="<?php print $value['item_id']; ?>">
-             </form>
-         </th>
-				<th>
-					<form method="post">
-						<input type="text" class="input_text_width"
-							name="update_price" value="<?php print $value['price']; ?>">円<input
-							type="submit" value="価格変更"> <input type="hidden" name="item_id"
-							value="<?php print $value['item_id']; ?>"> <input type="hidden"
-							name="sql_kind" value="update_price">
-					</form>
-				</th>
-				<th>
-					<form method="post">
-						<input type="text" class="input_text_width text_align_right"
-							name="update_stock" value="<?php print $value['stock']; ?>">個<input
-							type="submit" value="変更"> <input type="hidden" name="item_id"
-							value="<?php print $value['item_id']; ?>"> <input type="hidden"
-							name="sql_kind" value="update_stock">
-					</form>
-				</th>
-				<th>
-					<form method="post">
-						<input type="submit" value="削除"> <input type="hidden"
-							name="sql_kind" value="delete"> <input type="hidden"
-							name="item_id" value="<?php print $value['item_id']; ?>">
-					</form>
-				</th>
-				<th>
->>>>>>> 48365b6... コミットメッセージ
                         <?php if ( $value['status'] === '1' ) { ?>
-                    <form method="post">
-						<input type="submit" value="公開→非公開"> <input type="hidden"
-							name="sql_kind" value="change"> <input type="hidden"
-							name="change_status" value="0"> <input type="hidden"
-							name="item_id" value="<?php print $value['item_id']; ?>">
-					</form>
-                        <?php } elseif ( $value['status'] === '0' ) { ?>
-                    <form method="post">
-						<input type="submit" value="非公開→公開"> <input type="hidden"
-							name="sql_kind" value="change"> <input type="hidden"
-							name="change_status" value="1"> <input type="hidden"
-							name="item_id" value="<?php print $value['item_id']; ?>">
-					</form>
+                        <form method="post">
+                            <input type="submit" value="公開→非公開">
+                            <input type="hidden" name="sql_kind" value="change">
+                            <input type="hidden" name="change_status" value="0">
+                            <input type="hidden" name="item_id" value="<?php print $value['item_id']; ?>">
+                        </form>
 
-				</th>
-			</tr>
+                        <?php } elseif ( $value['status'] === '0' ) { ?>
+                        <form method="post">
+                            <input type="submit" value="非公開→公開">
+                            <input type="hidden" name="sql_kind" value="change">
+                            <input type="hidden" name="change_status" value="1">
+                            <input type="hidden" name="item_id" value="<?php print $value['item_id']; ?>">
+                        </form>
+
+                    </th>
+                </tr>
 
                 <?php  } } ?>
         </table>
-	</section>
-	<!--?php var_dump($_POST['sql_kind']); ? -->
+    </section>
 </body>
 </html>
